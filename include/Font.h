2a168740396e740b9eaec3c9ee7728c6667b6595
habitate <rokas.post@gmail.com>
2019-08-06 19:00:07 +0300

    Small rework on the font class. Added a to-do list

--- include/Font.h
+++ include/Font.h
@@ -10,22 +10,19 @@ class Font{
         Font() noexcept;
         Font(const std::string& path, const unsigned int size = 14);
 
-        bool good() const noexcept;
-
         void load(const std::string& path, const unsigned int size = 14);
 
-        void setSize(const unsigned int size);
-        void setFont(const std::string& path);
+        void set_size(const unsigned int size);
+        void set_font(const std::string& path);
 
-        unsigned int getSize() const noexcept;
-        const std::string& getPath() const noexcept;
+        unsigned int get_size() const noexcept;
+        const std::string& get_path() const noexcept;
 
         void draw_text(SDL_Renderer* const renderer, const std::string& text, const int x, const int y, const SDL_Color& color) const;
         std::shared_ptr<SDL_Texture> render_text(SDL_Renderer* const renderer, const std::string& text, const SDL_Color& color) const;
 
+        bool good() const noexcept;
     private:
-        void update();
-
         std::string path;
         unsigned int size;
 
--- src/Font.cpp
+++ src/Font.cpp
@@ -6,80 +6,58 @@
 #include "color.h"
 
 Font::Font() noexcept: path(), size(0), font(nullptr, TTF_CloseFont) {}
-Font::Font(const std::string& path, const unsigned int size) : path(), size(0), font(nullptr, TTF_CloseFont){
+Font::Font(const std::string& path, const unsigned int size) : Font(){
     load(path, size);
 }
 
-bool Font::good() const noexcept{
-    return (bool)font;
-}
-
 void Font::load(const std::string& path, const unsigned int size){
-    //? Prevent unnecessary reloading
+    //? Prevent unnecessary loading
     if(this->path == path && this->size == size){
         return;
     }
 
     this->path = path;
     this->size = size;
-    update();
-}
-
-void Font::setSize(const unsigned int size){
-    //? Prevent unnecessary reloading
-    if(this->size == size){
-        return;
-    }
-
-    this->size = size;
-    update();
-}
-void Font::setFont(const std::string& path){
-    //? Prevent unnecessary reloading
-    if(this->path == path){
-        return;
-    }
-
-    this->path = path;
-    update();
-}
-
-unsigned int Font::getSize() const noexcept{
-    return size;
-}
-const std::string& Font::getPath() const noexcept{
-    return path;
-}
-
-void Font::update(){
+    
     font.reset(TTF_OpenFont(path.c_str(), (int)size));
 
     if(!font){
         std::cout << C(C::RED) << "Failed loading font: " << C(C::CYAN) << '\"'<< path << "\"\n" << C();
         std::cout << "\tError: " << SDL_GetError() << '\n';
 
-        throw std::runtime_error("^^^");
+        throw std::runtime_error("");
     }
     else{
         std::cout << C(C::GREEN) << "Successfully loaded font: " << C(C::CYAN) << "\"" << path << "\"\n" << C();
     }
 }
+void Font::set_size(const unsigned int size){
+    load(this->path, size);
+}
+void Font::set_font(const std::string& path){
+    load(path, this->size);
+}
+
+unsigned int Font::get_size() const noexcept{
+    return size;
+}
+const std::string& Font::get_path() const noexcept{
+    return path;
+}
 
 void Font::draw_text(SDL_Renderer* const renderer, const std::string& text, const int x, const int y, const SDL_Color& color) const{
     if(!font){
-        throw std::runtime_error((std::string("Failed to draw text " + text + "\n\tTTF Errors: " + TTF_GetError() + "!\n")).c_str());
+        throw std::runtime_error("Tried to draw text from an uninitialized font!\n");
     }
 
     const std::unique_ptr<SDL_Surface, void(*)(SDL_Surface*)> loader(TTF_RenderText_Blended(this->font.get(), text.c_str(), color), SDL_FreeSurface);
-    
     if(!loader){
-        throw std::runtime_error((std::string("Failed to draw text " + text + "\n\tTTF Errors: " + TTF_GetError() + "!\n")).c_str());
+        throw std::runtime_error(std::string("TTF_RenderText_Blended failed when drawing text!\n\t   Error: ") + SDL_GetError() + '\n');
     }
 
     const std::unique_ptr<SDL_Texture, void(*)(SDL_Texture*)> sprite(SDL_CreateTextureFromSurface(renderer, loader.get()), SDL_DestroyTexture);
-
     if(!sprite){
-        throw std::runtime_error((std::string("Failed to draw text " + text + "\n\tTTF Errors: " + TTF_GetError() + "!\n")).c_str());
+        throw std::runtime_error(std::string("SDL_CreateTextureFromSurface failed when drawing text!\n\t   Error: ") + SDL_GetError() + '\n');
     }
     
     const SDL_Rect srcRect = {0, 0, loader->w, loader->h};
@@ -90,20 +68,22 @@ void Font::draw_text(SDL_Renderer* const renderer, const std::string& text, cons
 
 std::shared_ptr<SDL_Texture> Font::render_text(SDL_Renderer* const renderer, const std::string& text, const SDL_Color& color) const{
     if(!font){
-        throw std::runtime_error((std::string("Failed to render text " + text + "\n\tTTF Errors: " + TTF_GetError() + "!\n")).c_str());
+        throw std::runtime_error("Tried to draw text from an uninitialized font!\n");
     }
 
     const std::unique_ptr<SDL_Surface, void(*)(SDL_Surface*)> loader(TTF_RenderText_Blended(this->font.get(), text.c_str(), color), SDL_FreeSurface);
-    
     if(!loader){
-        throw std::runtime_error((std::string("Failed to render text " + text + "\n\tTTF Errors: " + TTF_GetError() + "!\n")).c_str());
+        throw std::runtime_error(std::string("TTF_RenderText_Blended failed when rendering text!\n\t   Error: ") + SDL_GetError() + '\n');
     }
 
     const std::shared_ptr<SDL_Texture> sprite(SDL_CreateTextureFromSurface(renderer, loader.get()), SDL_DestroyTexture);
-
     if(!sprite){
-        throw std::runtime_error((std::string("Failed to render text " + text + "\n\tTTF Errors: " + TTF_GetError() + "!\n")).c_str());
+        throw std::runtime_error(std::string("SDL_CreateTextureFromSurface failed when rendering text!\n\t   Error: ") + SDL_GetError() + '\n');
     }
     
     return sprite;
+}
+
+bool Font::good() const noexcept{
+    return (bool)font;
 }
